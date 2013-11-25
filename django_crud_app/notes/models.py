05c0e5c8aa3891105235e9e6848c31cca0dab1b8
Ben Grunfeld <benjamin.grunfeld@gmail.com>
2013-11-25 11:45:38 -0700

    Added model for Notes app. Changed timezone to America/Denver

--- django_crud_app/notes/models.py
+++ django_crud_app/notes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	first_published_date = models.DateTimeField('Date Published')
+	last_update_date = models.DateTimeField('Last Update')
+
+	def __unicode__(self):  # Python 3: def __str__(self):
+		return self.content
