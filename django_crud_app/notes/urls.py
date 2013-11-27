3cb00ee1344d351f4dcbdb238af77f0ba416ae6d
Ben Grunfeld <benjamin.grunfeld@gmail.com>
2013-11-27 12:41:27 -0700

    Added DELETE view to views.py and delete functionality to template. Tested and works.

--- django_crud_app/notes/urls.py
+++ django_crud_app/notes/urls.py
@@ -4,5 +4,6 @@ from notes import views
 
 urlpatterns = patterns('', 
 	url(r'^$', views.index, name='index'),
-	url(r'^(?P<note_id>\d+)/$', views.read, name='read')
+	url(r'^(?P<note_id>\d+)/$', views.read, name='read'),
+	url(r'^(?P<note_id>\d+)/delete/$', views.delete, name='delete')
 )
--- django_crud_app/notes/views.py
+++ django_crud_app/notes/views.py
@@ -5,25 +5,21 @@ from notes.models import Note
 def index(request):
 	"""Lists all of the Notes in the Database"""
 	list_of_notes = Note.objects.all().order_by('-last_update_date')
-	if not list_of_notes:
-		template = 'notes/empty.html'
-	else:
-		template = 'notes/base.html'
+	template = 'notes/base.html'
 	target = list_of_notes[0]
 	context = {'list_of_notes': list_of_notes, 'target': target}
 	return render(request, template, context)
 
 def create(request, note_id):
-	"""Creates a new note in the Database"""
+	# """Creates a new note in the Database"""
+	# list_of_notes = Note.objects.all().order_by('-last_update_date')
+	# template = 'notes/base.html'
 	pass
 
 def read(request, note_id):
 	"""Reads a specific note from the Database"""
 	list_of_notes = Note.objects.all().order_by('-last_update_date')
-	if not list_of_notes:
-		template = 'notes/empty.html'
-	else:
-		template = 'notes/base.html'
+	template = 'notes/base.html'
 	target = Note.objects.get(id=note_id)
 	context = {'list_of_notes': list_of_notes, 'target': target}
 	return render(request, template, context)
@@ -34,4 +30,12 @@ def update(request, note_id):
 
 def delete(request, note_id):
 	"""Deletes a specific note from the Database"""
-	pass
+	list_of_notes = Note.objects.all().order_by('-last_update_date')
+	template = 'notes/base.html'
+	target = Note.objects.get(id=note_id)
+	tempid = target.id
+	target.delete()
+	deleted = True
+	# messages add "Object was deleted"
+	context = {'deleted': deleted, 'tempid': tempid, 'list_of_notes': list_of_notes}
+	return render(request, template, context)
