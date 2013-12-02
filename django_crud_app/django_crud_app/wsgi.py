a5d5ced8477cab1a88ff58d127d92db9d7a08bf8
Ben Grunfeld <benjamin.grunfeld@gmail.com>
2013-12-02 10:07:55 -0700

    Updated .gitignore to ignore .pyc files.

--- django_crud_app/django_crud_app/wsgi.py
+++ django_crud_app/django_crud_app/wsgi.py
@@ -12,3 +12,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_crud_app.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+# Bind to PORT if defined, otherwise default to 5000.
+# port = int(os.environ.get('PORT', 5000))
+# notes.run(host='0.0.0.0', port=port)	
